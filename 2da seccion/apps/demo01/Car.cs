c9f7227705d8162c9e2336e23ef0cb923e5004bd
josuered4 <josuered4@gmail.com>
2023-02-25 12:59:59 -0600

    2da Seccion

---
+++ 2da seccion/apps/demo01/Car.cs
@@ -0,0 +1,18 @@
+public class Car
+{
+    //Las clases por default son internal: para el mismo ensamblado 
+    //Aqui van los Campos, Métodos, Propiedades y Eventos.
+
+
+
+    private class Auto
+    {
+        //Los componentes de este mismo solo podran ser accedidos por esta clase 
+        //este tipo de clases solo pueden ser creadas dentro de otras clases 
+        private string Model; //=>todo campo deberia ser privado
+
+        
+        
+    }
+
+}
---
+++ 2da seccion/apps/demo01/Program.cs
@@ -0,0 +1,63 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello Word");
+        var Cuadrado = new Square();
+        Cuadrado.Side = 23;
+        Console.WriteLine(Cuadrado.Side);
+
+        Cuadrado.Num = 10;
+        Console.WriteLine(Cuadrado.Num);
+    }
+}
+
+class Square
+{
+    //Aqui van los Campos, Métodos, Propiedades y Eventos.
+    //Campos(Fields)
+    private decimal Side_BackingField; //=> las clases deben ser privados por defaul
+
+    //Propiedades 
+        //MEtodos especiales que no ayudan a proteger nuestros campos de las clases
+        //que consumen de esta clase.
+    public decimal Side //=> la definicion es similar a la declaracion de una variable
+    {
+        set{ Side_BackingField = value; }//Asigna valor al BackingField
+        get{ return Side_BackingField; }
+    }
+
+    //propiedades 
+    //Aun se requiere el BackingField, aunque no lo pongamos
+    //el compilador implementa un campo para esta propiedad e implementa 
+    //esta propiedad
+    public decimal Side2{get; private set;}
+    //get publico y get provado
+    public void SetSide2Value(decimal d){
+        Side2 = d; //asi se le pasaria el salor al set
+    }
+
+    public decimal Side3{get;} //=> Solo en el contructor puede y debe ser inicializado 
+
+    //Campo
+    private decimal Area_BF;
+    //propiedad de solo lectura 
+    public decimal Area
+    {
+        get{return Area_BF;}
+    }
+
+
+    //Campo 
+    private int Num_BF;
+    public int Num
+    {
+        set{
+            if(value < 5)
+                Num_BF = value;
+            else
+                Num = 0;
+        }
+        get{return Num_BF;}
+    }
+}
